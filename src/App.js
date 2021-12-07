624beb58c586b300319265e119c9df86d21fb84f
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-07 17:00:07 -0600

    Finished styling sidebar draft

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar"
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="content">
+        <Sidebar />
+        <div className="main">Main</div>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from "react";
+import "./sidebar.scss";
+import { Home, TrendingUp, Insights,
+         PermIdentity, Storefront, AttachMoney, BarChart,
+         MailOutline, Feedback, ChatBubbleOutline,
+        WorkOutline, Report
+
+        } from "@mui/icons-material/";
 
 const Sidebar = () => {
-    return (
-        <div>
-            sidebar
+  return (
+    <div className="sidebarContainer">
+      <div className="sidebarContent">
+        <div className="sidebarMenu">
+          <h4 className="sidebarTitle">Dashboard</h4>
+          <ul className="sidebarList">
+            <li>
+              <Home className="sidebarIcon"/>
+              Home
+            </li>
+            <li>
+              <Insights className="sidebarIcon"/>
+              Analytics
+            </li>
+            <li>
+              <TrendingUp className="sidebarIcon"/>
+              Sales
+            </li>
+          </ul>
         </div>
-    )
-}
+        <div className="sidebarMenu">
+          <h4 className="sidebarTitle">Quick Menu</h4>
+          <ul className="sidebarList">
+            <li>
+              <PermIdentity className="sidebarIcon"/>
+              Users
+            </li>
+            <li>
+              <Storefront className="sidebarIcon"/>
+              Products
+            </li>
+            <li>
+              <AttachMoney className="sidebarIcon"/>
+              Transactions
+            </li>
+            <li>
+              <BarChart className="sidebarIcon"/>
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h4 className="sidebarTitle">Notifications</h4>
+          <ul className="sidebarList">
+            <li>
+              <MailOutline className="sidebarIcon"/>
+              Mail
+            </li>
+            <li>
+              <Feedback className="sidebarIcon"/>
+              Analytics
+            </li>
+            <li>
+              <ChatBubbleOutline className="sidebarIcon"/>
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h4 className="sidebarTitle">Staff</h4>
+          <ul className="sidebarList">
+            <li>
+              <WorkOutline className="sidebarIcon"/>
+              Manage
+            </li>
+            <li>
+              <Insights className="sidebarIcon"/>
+              Analytics
+            </li>
+            <li>
+              <Report className="sidebarIcon"/>
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
