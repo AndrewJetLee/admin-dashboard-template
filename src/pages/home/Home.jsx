6ac58e732902021110e9c461cfe129e8548372d7
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-09 21:17:23 -0600

    Finished styling homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import "./home.scss"
-import FeaturedInfo from '../../components/FeaturedInfo'
-import Chart from '../../components/Chart'
-import { userData } from "../../dummyData"
+import React from "react";
+import "./home.scss";
+import FeaturedInfo from "../../components/FeaturedInfo";
+import Chart from "../../components/Chart";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/WidgetLg";
+import WidgetSm from "../../components/WidgetSm";
 
 const Home = () => {
-    return (
-        <div className="homeContainer">
-            <FeaturedInfo/>
-            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-            <div className="homeWidget">Home Widget</div>
-        </div>
-    )
-}
+  return (
+    <div className="homeContainer">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
