42f97b8c9cfc38357cfb67048de82426634bf3e9
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-10 14:25:55 -0600

    Created nested functional component for WidgetLgRow for DRY code

--- src/components/WidgetLg.jsx
+++ src/components/WidgetLg.jsx
@@ -1,60 +1,47 @@
-import "./widgetLg.scss"
+import "./widgetLg.scss";
 
 const WidgetLg = () => {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
 
-    const Button = ({type}) => {
-        return <button className={"widgetLgButton " + type}>{type}</button>
-    }
-
+  const WidgetLgRow = ({status}) => {
     return (
-        <div className="widgetLgContainer">
-            <h3 className="widgetLgTitle">Latest Transactions</h3>
-            <table className="widgetLgTable">
-                <tr className="widgetLgTr">
-                    <th className="widgetLgTh">Customer</th>
-                    <th className="widgetLgTh">Date</th>
-                    <th className="widgetLgTh">Amount</th>
-                    <th className="widgetLgTh">Status</th>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img className="widgetLgImg" src="http://placekitten.com/200/300" alt="" />
-                        <span className="widgetLgName">Susan Carol</span>
-                    </td>
-                    <td className="widgetLgDate">2 Jun 2021</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Approved"/></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img className="widgetLgImg" src="http://placekitten.com/200/300" alt="" />
-                        <span className="widgetLgName">Susan Carol</span>
-                    </td>
-                    <td className="widgetLgDate">2 Jun 2021</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Pending"/></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img className="widgetLgImg" src="http://placekitten.com/200/300" alt="" />
-                        <span className="widgetLgName">Susan Carol</span>
-                    </td>
-                    <td className="widgetLgDate">2 Jun 2021</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Declined"/></td>
-                </tr>
-                <tr className="widgetLgTr">
-                    <td className="widgetLgUser">
-                        <img className="widgetLgImg" src="http://placekitten.com/200/300" alt="" />
-                        <span className="widgetLgName">Susan Carol</span>
-                    </td>
-                    <td className="widgetLgDate">2 Jun 2021</td>
-                    <td className="widgetLgAmount">$122.00</td>
-                    <td className="widgetLgStatus"><Button type="Approved"/></td>
-                </tr>
-            </table>
-        </div>
-    )
-}
+      <tr className="widgetLgTr">
+        <td className="widgetLgUser">
+          <img
+            className="widgetLgImg"
+            src="http://placekitten.com/200/300"
+            alt=""
+          />
+          <span className="widgetLgName">Susan Carol</span>
+        </td>
+        <td className="widgetLgDate">2 Jun 2021</td>
+        <td className="widgetLgAmount">$122.00</td>
+        <td className="widgetLgStatus">
+          <Button type={status} />
+        </td>
+      </tr>
+    );
+  };
 
-export default WidgetLg
+  return (
+    <div className="widgetLgContainer">
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <WidgetLgRow status="Approved"/>
+        <WidgetLgRow status="Declined"/>
+        <WidgetLgRow status="Pending"/>
+        <WidgetLgRow status="Approved"/>
+      </table>
+    </div>
+  );
+};
+
+export default WidgetLg;
